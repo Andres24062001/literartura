fdd96cc163abd10b77b65919ad3fa5c93b2ec76e
Andres Manrique <andrsmmsa1@gmail.com>
2024-06-04 15:10:24 -0400

    iniciando proyecto

---
+++ src/main/java/com/alura/literartura/LiterarturaApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literartura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterarturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterarturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/literartura/LiterarturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literartura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterarturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
